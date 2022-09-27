f9fc13db24665803193902a1fc103ac4c1263f3a
outdoorhosen24-de <outdoorhosen24-de@users.noreply.github.com>
2022-09-27 12:35:37 +0000

    deploy: 5f2285236915a0b083a48d6919d38f573a9c7d30

---
+++ assets/static/1664282077/marken/globo/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/marken/globo", {data:[{_img:{"/_ipx/f_webp,q_90,s_407x229/assets/images/images/globo-deckenventilator-ugo-nickel-matt-globo-3938222-1.jpg":"\u002Fassets\u002Fimages\u002Fglobo-deckenventilator-ugo-nickel-matt-globo-3938222-1.webp"}}],fetch:{},mutations:void 0});
---
+++ assets/static/1664282077/marken/vasner/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/marken/vasner", {data:[{_img:{"/_ipx/f_webp,q_90,s_407x229/assets/images/images/vasner-hybridheizung-hybrid-infrarotheizung-konvektionsheizung-konvi-1200-watt-vasner-6979843-1.jpg":"\u002Fassets\u002Fimages\u002Fvasner-hybridheizung-hybrid-infrarotheizung-konvektionsheizung-konvi-1200-watt-vasner-6979843-1.webp"}}],fetch:{},mutations:void 0});
